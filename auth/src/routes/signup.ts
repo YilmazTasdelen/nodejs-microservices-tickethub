72077d0363d4c373deb91bbd16049ad5e4dc75e8
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-21 17:00:16 +0300

    .

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,28 @@
-import express from 'express';
-
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 const router = express.Router();
 
-router.post('/ai/users/signup', (req, res) => {
+router.post('/ai/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('email must be valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('password must be between 4 -20 characters')
+
+
+], (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+    console.log('creating user...');
+
+
     res.send('Hi there');
 });
 
